05da3f9d45878a2b83b3cf246f1420280bed394d
wangtao913 <forestWt@126.com>
2019-09-19 18:00:11 +0800

    接口联调

--- src/api/index.js
+++ src/api/index.js
@@ -1,5 +1,5 @@
-// import { post, get } from '../utils/request'
-import { post } from '../utils/request'
+import { post, get } from '../utils/request'
+// import { post } from '../utils/request'
 const BASE_URL = 'http://col.gsgouma.com'
 /* 获取商家详情 */
 export function getUser(params) {
@@ -7,5 +7,5 @@ export function getUser(params) {
 }
 /* 小程序登录授权管理 */
 export function loginWx(params) {
-  return post(`${BASE_URL}/api/login/loginWx`, params)
+  return get(`${BASE_URL}/api/login/loginWx`, params)
 }
